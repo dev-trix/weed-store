cf77ee06c6a68e058257366e0877cc4725a0ef5d
Dev-Trix <adeseovie@gmail.com>
2019-11-25 18:45:08 -0500

    Webpack production

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
 // entry point -> output
 const path = require("path");
-console.log(path.join(__dirname, "public"));
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_module/
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public")
-  }
+
+module.exports = env => {
+  const isProduction = env === "production";
+
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_module/
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"]
+        }
+      ]
+    },
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public")
+    }
+  };
 };
 
 // loader
