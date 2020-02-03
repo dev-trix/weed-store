054dd007326a4e567db6d0f596088b0e164457fb
Dev-Trix <adeseovie@gmail.com>
2020-02-03 15:29:42 -0500

    added payment

--- server/server.js
+++ server/server.js
@@ -12,3 +12,29 @@ app.get("*", (req, res) => {
 app.listen(3001, () => {
   console.log("Server is up!");
 });
+
+//Paymentpage Route
+app.post("/charge", function(req, res) {
+  var stripeToken = req.body.stripeToken;
+  var finalPrice = app.get("price");
+  console.log(finalPrice.slice(1, -1));
+  const productPrice = parseInt(finalPrice.slice(1, -1)) * 1000;
+  (async function() {
+    // Create a Customer:
+
+    // Charge the Customer instead of the card:
+    const charge = await stripe.charges.create(
+      {
+        amount: productPrice,
+        currency: "cad",
+        source: "tok_visa", // obtained with Stripe.js
+        description: "Charge for Dev-Trix.com"
+      },
+      function(err, charge) {
+        // asynchronously called
+      }
+    );
+
+    // YOUR CODE: Save the customer ID and other info in a database for later.
+  })();
+});
